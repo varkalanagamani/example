5c0338a33314ee30c7178b7164404f8db2298718
varkalanagamani <varkalanagamani@gmail.com>
2023-08-29 12:48:49 +0530

    jwt token example

---
+++ jwt/spring-boot-jwt-master/src/main/java/com/techgeeknext/SpringBootJwtApplication.java
@@ -0,0 +1,14 @@
+package com.techgeeknext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtApplication.class, args);
+		System.out.println("enterd into main method...........");
+	}
+
+}
---
+++ jwt/spring-boot-jwt-master/src/main/java/com/techgeeknext/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.techgeeknext.config;
+
+import java.io.IOException;
+
+import java.io.Serializable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+	
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	private static final long serialVersionUID = -7858869558953243875L;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+		logger.info("JwtAuthenticationEntryPoint.class");
+
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+	}
+}
